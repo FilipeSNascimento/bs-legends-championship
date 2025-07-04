e8d82651548a15c9f0a8b56678e67c8155f6448b
Filipe Silva do Nascimento <123flipe.nasc3@gmail.com>
2025-07-03 21:33:15 -0300

    ajustei as partials

--- serverbs.js
+++ serverbs.js
@@ -48,7 +48,7 @@ app.get('/inscricao', (req, res) => {
 });
 
 //Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
